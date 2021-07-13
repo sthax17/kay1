1d52554c1fd78d3fd232d04bc4db18682021ca27
sthax17 <sthax17@gmail.com>
2021-07-13 16:39:20 +0900

    07/13 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -9,19 +9,21 @@ from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
+
 def hello_world(request):
     if request.method == "POST":
 
-       temp = request.POST.get('hello_world_input')
-       new_hello_world = HelloWorld()
-       new_hello_world.text = temp
-       new_hello_world.save()
+        temp = request.POST.get('hello_world_input')
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
 
-       return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-       hello_world_list = HelloWorld.objects.all()
-       return render(request, 'accountapp/hello_world.html',
-                     context={'hello_world_list': hello_world_list})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html',
+                      context={'hello_world_list': hello_world_list})
+
 
 class AccountCreatView(CreateView):
     model = User
